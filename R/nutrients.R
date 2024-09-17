33a5d76e3df9a62ffc25ad2a0f945532dc69b98f
Travis Hinkelman <thinkelman@esassoc.com>
2024-09-16 17:54:37 -0700

    Actually fix bug in prep_nut_site

--- R/nutrients.R
+++ R/nutrients.R
@@ -89,7 +89,7 @@ prep_nut_site <- function(yr, nut){
     dplyr::right_join(nut)
 
   if (yr == "2023"){
-    nut_site = mutate(nut_site, group_b_method = get_gbm(nut_site))
+    nut_site = dplyr::mutate(nut_site, group_b_method = get_gbm(group_b_name))
   }
   nut_site
 }
